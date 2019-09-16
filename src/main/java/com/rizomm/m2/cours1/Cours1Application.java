85c70c3a6e82d2ec5acc58f80db2eaa729d947b5
Anthony Callaert <anthony.callaert@decathlon.com>
2019-09-16 21:51:41 +0200

    init sample

---
+++ src/main/java/com/rizomm/m2/cours1/Cours1Application.java
@@ -0,0 +1,13 @@
+package com.rizomm.m2.cours1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cours1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cours1Application.class, args);
+	}
+
+}
