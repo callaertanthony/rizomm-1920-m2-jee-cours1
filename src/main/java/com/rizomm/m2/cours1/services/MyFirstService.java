d1035209545f877b1c4d3ef18fd3bec4ab6f4161
Anthony Callaert <anthony.callaert@decathlon.com>
2019-09-18 09:55:32 +0200

    first demo

---
+++ src/main/java/com/rizomm/m2/cours1/services/MyFirstService.java
@@ -0,0 +1,7 @@
+package com.rizomm.m2.cours1.services;
+
+public interface MyFirstService {
+
+  String helloWorld();
+
+}
--- src/test/java/com/rizomm/m2/cours1/Cours1ApplicationTests.java
+++ src/test/java/com/rizomm/m2/cours1/Cours1ApplicationTests.java
@@ -11,6 +11,7 @@ public class Cours1ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
