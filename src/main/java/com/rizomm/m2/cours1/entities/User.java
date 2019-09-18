a1317463b477c8aa6584788bf6001bfa38d1f2b4
Anthony Callaert <anthony.callaert@decathlon.com>
2019-09-18 10:11:12 +0200

    add user class

---
+++ src/main/java/com/rizomm/m2/cours1/entities/User.java
@@ -0,0 +1,15 @@
+package com.rizomm.m2.cours1.entities;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+
+  private String firstName;
+
+  private String lastName;
+
+  private String mail;
+
+}
